43cf16542441e6f851764b9ba6d68193ffa779d3
AramisB <brendachem7@gmail.com>
2023-10-05 09:33:33 -0700

    size in bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Description: main - using sizeof
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %d bytes.\n", sizeof(char));
+  printf("Size of a short int: %d bytes.\n", sizeof(short int));
+  printf("Size of an int: %d bytes.\n", sizeof(int));
+  printf("Size of a long int: %d bytes.\n", sizeof(long int));
+  printf("Size of a long long int: %d bytes.\n", sizeof(long long int));
+  printf("Size of a float: %d bytes.\n", sizeof(float));
+return (0);
+}
