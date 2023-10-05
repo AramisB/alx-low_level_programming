895722cc1d0d459e55bc6c9f07f217e4f92610a7
AramisB <brendachem7@gmail.com>
2023-10-05 08:42:40 -0700

    using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: Prints a string to output using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
