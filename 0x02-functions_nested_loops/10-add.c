21066a6228723031a24654f37e01d1ab8a8bdcb5
AramisB <brendachem7@gmail.com>
2023-10-11 11:03:23 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add -adds two digits
+ *
+ * @a - first int
+ * @b - second int
+ *
+ * Returns: sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return(sum);
+}
