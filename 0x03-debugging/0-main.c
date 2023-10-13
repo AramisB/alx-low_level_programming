5c93c6965c0891562cae0dafef74b49b83151276
AramisB <brendachem7@gmail.com>
2023-10-13 05:39:42 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - test for positive or negative value
+ *
+ * Return: Always 0(success)
+ *
+ */
+int main(void)
+{
+	int i = 0;
+
+	printf("%d", i);
+
+	positive_or_negative(i);
+
+	return(0);
+}
