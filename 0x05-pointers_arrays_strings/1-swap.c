7b960775545e15d9b3c3edde82cb4dd8e984b8cb
AramisB <brendachem7@gmail.com>
2023-10-16 06:51:44 -0700

    length of string

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -9,6 +9,9 @@
  */
 void swap_int(int *a, int *b)
 {
-	*a = 42;
-	*b = 98;
+	int y;
+
+	y = *a;
+	*a = *b;
+	*b = y;
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
+
+#include <stdio.h>
+#include <unistd.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
