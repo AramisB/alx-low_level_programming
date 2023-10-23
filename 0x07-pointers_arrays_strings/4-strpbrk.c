47d5ededc2277234edd9b8a68cc1b20d73ce22fd
AramisB <brendachem7@gmail.com>
2023-10-23 08:59:12 -0700

    string pointer break

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - search for the ccurrence of any character from a specified set
+ *
+ * @s: string
+ * @accept: string with specified characters
+ *
+ * Return: Null
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
+}
