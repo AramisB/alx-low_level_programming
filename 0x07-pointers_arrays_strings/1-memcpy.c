d8f86a9e7238f58dcc57bdf9a8f01b9143c4eb22
AramisB <brendachem7@gmail.com>
2023-10-23 05:54:36 -0700

    copying bytes in unsigned int

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - copies a memory area
+ *
+ * @dest: where memory is copied
+ * @src: where memory is stored
+ * @n: number of bytes
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
