880243b3690616635b6bb029c21435e11b09267e
AramisB <brendachem7@gmail.com>
2023-10-27 08:19:30 -0700

    unused variable

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: arrays
+ *
+ * Return: Always 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("argc = %d\n", argc);
+	return (0);
+}
