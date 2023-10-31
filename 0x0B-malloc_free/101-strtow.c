52a8cb2dfc025a5378172b0d8b2ab08020ec43a5
AramisB <brendachem7@gmail.com>
2023-10-31 13:13:12 -0700

    malloc and free fxns

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,82 +9,72 @@
  */
 int count_word(char *s)
 {
-    int flag = 0, c = 0, w = 0;
+int flag = 0, c = 0, w = 0;
 
-    for (; s[c] != '\0'; c++)
-    {
-        if (s[c] == ' ')
-            flag = 0;
-        else if (flag == 0)
-        {
-            flag = 1;
-            w++;
-        }
-    }
-
-    return w;
+for (; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+	flag = 0;
+else if (flag == 0)
+{
+flag = 1;
+w++;
+}
+}
+return (w);
 }
 
 /**
- * **strtow - splits a string into words
+ * strtow - splits a string into words
  * @str: string to split
  *
  * Return: pointer to an array of strings (Success) or NULL (Error)
  */
 char **strtow(char *str)
 {
-    char **matrix, *tmp;
-    int i, k = 0, len = 0, words, c = 0, start = 0, end = 0;
+char **matrix, *tmp;
+int i, k = 0, len = 0, words, c = 0, start = 0, end = 0;
 
-    while (str[len])
-        len++;
+while (str[len])
+	len++;
+words = count_word(str);
+if (words == 0)
+	return (NULL);
+matrix = (char **)malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+	return (NULL);
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *)malloc(sizeof(char) * (c + 1));
 
-    words = count_word(str);
-
-    if (words == 0)
-        return NULL;
-
-    matrix = (char **)malloc(sizeof(char *) * (words + 1));
-
-    if (matrix == NULL)
-        return NULL;
-
-    for (i = 0; i <= len; i++)
-    {
-        if (str[i] == ' ' || str[i] == '\0')
-        {
-            if (c)
-            {
-                end = i;
-                tmp = (char *)malloc(sizeof(char) * (c + 1));
-
-                if (tmp == NULL)
-                    return NULL;
-
-                while (start < end)
-                {
-                    *tmp++ = str[start];
-                    start++;
-                }
-
-                *tmp = '\0';
-                matrix[k] = tmp - c;
-                k++;
-                c = 0;
-            }
-        }
-        else if (i == 0 || (str[i - 1] == ' ' && i > 0))
-        {
-            start = i;
-            c = 1;
-        }
-        else
-        {
-            c++;
-        }
-    }
-
-    matrix[k] = NULL;
-
-    return matrix;
+if (tmp == NULL)
+	return (NULL);
+while (start < end)
+{
+*tmp++ = str[start];
+start++;
+}
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (i == 0 || (str[i - 1] == ' ' && i > 0))
+{
+start = i;
+c = 1;
+}
+else
+{
+c++;
+}
+}
+matrix[k] = NULL;
+return (matrix);
 }
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,43 +10,26 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    int i, ci;
+char *concat;
+int len1 = 0, len2 = 0, i, j;
 
-    if (s1 == NULL)
-        s1 = "";
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+while (s1[len1])
+	len1++;
+while (s2[len2])
+	len2++;
 
-    if (s2 == NULL)
-        s2 = "";
+concat = malloc(sizeof(char) * (len1 + len2 + 1));
 
-    i = ci = 0;
-
-    while (s1[i] != '\0')
-        i++;
-
-    while (s2[ci] != '\0')
-        ci++;
-
-    concat = malloc(sizeof(char) * (i + ci + 1));
-
-    if (concat == NULL)
-        return (NULL);
-
-    i = ci = 0;
-
-    while (s1[i] != '\0')
-    {
-        concat[i] = s1[i];
-        i++;
-    }
-
-    while (s2[ci] != '\0')
-    {
-        concat[i] = s2[ci];
-        i++, ci++;
-    }
-
-    concat[i] = '\0';
-
-    return (concat);
+if (concat == NULL)
+	return (NULL);
+for (i = 0; i < len1; i++)
+	concat[i] = s1[i];
+for (j = 0; j < len2; j++, i++)
+	concat[i] = s2[j];
+concat[i] = '\0';
+return (concat);
 }
