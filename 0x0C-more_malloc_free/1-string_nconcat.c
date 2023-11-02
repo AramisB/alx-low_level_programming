f0cf6eca397e60acd3c1836276a209d33049746c
AramisB <brendachem7@gmail.com>
2023-11-02 08:14:45 -0700

    more malloc and free fxns

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,34 +14,28 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int x, y, z;
 char *s;
 
-
 if (s1 == NULL)
 {
-	x = 0;
+	s1 = "";
 }
-else
+if (s2 == NULL)
+{
+	s2 = "";
+}
 for (x = 0; s1[x]; ++x)
 {
 	;
 }
-if (s2 == NULL)
-{
-	y = 0;
-}
-else
-for (y = 0; s2[y]; ++y)
+for (y = 0; s2[y] && y < n; ++y)
 {
 	;
 }
-}
-if (y > n)
-y = n;
-
 s = malloc(sizeof(char) * (x + y + 1));
 if (s == NULL)
 {
-	return (NULL);
+return (NULL);
 }
+
 for (z = 0; z < x; z++)
 {
 	s[z] = s1[z];
@@ -50,6 +44,7 @@ for (z = 0; z < y; z++)
 {
 	s[z + x] = s2[z];
 }
+
 s[x + y] = '\0';
 return (s);
 }
