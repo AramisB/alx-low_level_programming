9cd20b9679419091d27c12e37bf25326d4202944
AramisB <brendachem7@gmail.com>
2023-11-06 08:53:03 -0800

    structures and typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_dog - a fxn that frees sog
+ * @dog_t: dog to be freed
+ * @d:pointer to dog_t
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			free(d->name);
+		}
+		if (d->owner != NULL)
+		{
+			free(d->owner);
+		}
+		free(d);
+	}
+}
