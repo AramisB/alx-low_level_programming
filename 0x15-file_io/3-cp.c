4daf69756660fa94ecd9f60bc20cfceeb6d30ddf
AramisB <brendachem7@gmail.com>
2023-11-27 13:30:45 -0800

    copy file contents to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,24 @@
 #include "main.h"
-
+/**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
 /**
  * main - Copies contents of one file to another.
  *
@@ -19,39 +38,24 @@ int main(int argc, char *argv[])
         dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
-
     file_from = open(argv[1], O_RDONLY);
-    if (file_from == -1)
+    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
+    error_file(file_from, file_to, argv);
+    number1 = 1024;
+    
+    while (number1 == 1024)
     {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-        exit(98);
+	    number1 = read(file_from, buff, number1);
+	    if (number1 == -1)
+	    {
+		    error_file(-1, 0, argv);
+	    }
+	    number2 = write(file_to, buff, number1);
+	    if (number2 == -1)
+	    {
+		    error_file(0, -1, argv);
+	    }
     }
-
-    file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC,
-		    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (file_to == -1)
-    {
-        dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
-        exit(99);
-    }
-
-    do
-    {
-        number1 = read(file_from, buff, sizeof(buff));
-        if (number1 == -1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-            exit(98);
-        }
-
-        number2 = write(file_to, buff, number1);
-        if (number2 < number1)
-        {
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-            exit(99);
-        }
-    } while (number1 > 0);
-
     if (close(file_from) == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
