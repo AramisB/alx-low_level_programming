126fb298bd2b8351c4590acadab5a64e1b415be9
root <root@Chemu>
2024-02-02 07:19:23 -0600

    inserting index to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,13 +9,17 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	char *value_cp;
 	hash_node_t *new_node, *current;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-	{
 		return (0);
-	}
-	index = key_index((unsigned const char *)key, ht->size);
+	value_cp = strdup(value);
+
+	if (value_cp == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
 	while (current)
@@ -23,17 +27,15 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
-			current->value = strdup(value);
-			if (current->value == NULL)
-			{
-				return (0);
-			}
+			current->value = value_cp;
+			return (1);
 		}
 		current = current->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
+		free(value_cp);
 		return (0);
 	}
 	new_node->key = strdup(key);
@@ -42,13 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (0);
-	}
+	new_node->value = value_cp;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
