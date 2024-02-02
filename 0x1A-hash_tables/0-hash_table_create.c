de542bc74d4bf0fd28c49f7f539cea0941048990
root <root@Chemu>
2024-02-02 11:22:36 -0600

    create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned long int i;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
@@ -18,16 +17,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	table->size = size;
-	table->array = malloc(sizeof(hash_node_t *) * size);
+	table->array = calloc(size, sizeof(hash_node_t *));
 
 	if (table->array == NULL)
 	{
+		free(table);
 		return (NULL);
-	}
-
-	for (i = 0; i < table->size; i++)
-	{
-		table->array[i] = NULL;
 	}
 	return (table);
 }
